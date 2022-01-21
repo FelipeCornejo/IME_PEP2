2b7fd69e67352856d5d75dcb660d8f686ffe52a9
Felipe Cornejo <felipe.cornejo.i@usach.cl>
2022-01-21 19:04:32 -0300

    Pregunta 1 - lectura del datos

---
+++ Forma-05.R
@@ -0,0 +1,15 @@
+# Authors:
+#           Felipe Cornejo I. | 20.427.782-6
+#           Miguel Salinas G. | 20.215.515-4
+
+#PEP 2 IME - 2021-2
+
+#PREGUNTA 1.
+library(dplyr)
+library (tidyverse)
+library (ggpubr)
+library (ez)
+
+data <- read.csv(choose.files(), sep=";")
+
+alfa <- 0.01
